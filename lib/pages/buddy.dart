4adcdd4ab3931508e4cd4a407f1b672f46c6fa6d
Amit Amanna <amit.natheniel@gmail.com>
2025-01-26 15:30:54 +0100

    Load and sort best buddies based on user profile

--- lib/pages/buddy.dart
+++ lib/pages/buddy.dart
@@ -4,7 +4,6 @@ import 'package:case_study/widgets/formatted_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:case_study/models/subject.dart';
 
 class BuddyPage extends StatefulWidget {
   const BuddyPage({super.key});
@@ -14,21 +13,30 @@ class BuddyPage extends StatefulWidget {
 
 class _BuddyPageState extends State<BuddyPage> {
   Map<String, dynamic>? userData;
+  List<Map<String, dynamic>>? buddiesData;
   List<int> teachingSubjectIds = [];
   List<int> learningSubjectIds = [];
 
   @override
   void initState() {
     super.initState();
-    loadUserData();
+    loadJsonData();
     loadSavedSubjects();
   }
 
-  Future<void> loadUserData() async {
-    String jsonString = await rootBundle.loadString('assets/mock/user.json');
-    final jsonResponse = jsonDecode(jsonString);
+  Future<void> loadJsonData() async {
+    String userJsonString =
+        await rootBundle.loadString('assets/mock/user.json');
+    final userJsonResponse = jsonDecode(userJsonString);
+
+    String buddiesJsonString =
+        await rootBundle.loadString('assets/mock/buddies.json');
+    final buddiesJsonResponse =
+        List<Map<String, dynamic>>.from(jsonDecode(buddiesJsonString));
+
     setState(() {
-      userData = jsonResponse;
+      userData = userJsonResponse;
+      buddiesData = List<Map<String, dynamic>>.from(buddiesJsonResponse);
     });
   }
 
@@ -44,26 +52,93 @@ class _BuddyPageState extends State<BuddyPage> {
     });
   }
 
+  // This should be done on the Backend. Function just exists for demonstration purposes.
+  List<Map<String, dynamic>> sortBuddies() {
+    if (buddiesData == null ||
+        (teachingSubjectIds.isEmpty && learningSubjectIds.isEmpty)) {
+      return [];
+    }
+    List<Map<String, dynamic>> scoredBuddies = buddiesData!.map((buddy) {
+      num score = 0;
+
+      // Calculate matches for learningSubjectIds
+      score += buddy['learningSubjects']
+          .where((subject) => teachingSubjectIds.contains(subject))
+          .length;
+
+      // Calculate matches for teachingSubjects
+      score += buddy['teachingSubjects']
+          .where((subject) => learningSubjectIds.contains(subject))
+          .length;
+
+      // Add score to buddy data
+      return {
+        ...buddy,
+        'score': score,
+      };
+    }).toList();
+
+    // Sort buddies by score in descending order
+    scoredBuddies.sort((a, b) => b['score'].compareTo(a['score']));
+
+    return scoredBuddies;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final sortedBuddies = sortBuddies();
+
     return Scaffold(
       appBar: FormattedAppBar(title: "Buddy Page"),
       body: Center(
         child: userData == null
             ? CircularProgressIndicator()
-            : ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => UserPage(
-                          userData: userData!,
-                          teachingSubjectIds: teachingSubjectIds,
-                          learningSubjectIds: learningSubjectIds),
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => UserPage(
+                              userData: userData!,
+                              teachingSubjectIds: teachingSubjectIds,
+                              learningSubjectIds: learningSubjectIds),
+                        ),
+                      );
+                    },
+                    child: Text('Customise your Subject Preferences'),
+                  ),
+                  if (sortedBuddies.isNotEmpty)
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: DataTable(
+                          columns: [
+                            DataColumn(label: Text('Name')),
+                            DataColumn(label: Text('Teaching Subject IDs')),
+                            DataColumn(label: Text('Learning Subject IDs')),
+                            DataColumn(label: Text('Score')),
+                          ],
+                          rows: [
+                            for (var buddy in sortedBuddies)
+                              DataRow(
+                                cells: [
+                                  DataCell(Text(buddy['name'])),
+                                  DataCell(Text(buddy['teachingSubjects']
+                                      .map((e) => e.toString())
+                                      .join(', '))),
+                                  DataCell(Text(buddy['learningSubjects']
+                                      .map((e) => e.toString())
+                                      .join(', '))),
+                                  DataCell(Text(buddy['score'].toString())),
+                                ],
+                              ),
+                          ],
+                        ),
+                      ),
                     ),
-                  );
-                },
-                child: Text('Go to User Page'),
+                ],
               ),
       ),
     );
